e946964b52c19b8fadc13be2ead0bad406944f3a
jammudurgaprasad <163741538+jammudurgaprasad@users.noreply.github.com>
2024-09-17 22:01:39 +0530

    Add files via upload

---
+++ scripts2.js
@@ -0,0 +1,39 @@
+function about(){
+    document.getElementById("bt1").style.backgroundColor="rgba(255,1,136,255)";
+    document.getElementById("bt2").style.backgroundColor="transparent";
+    document.getElementById("bt3").style.backgroundColor="transparent";
+    document.getElementById("bt4").style.backgroundColor="transparent";
+    document.getElementById("bt5").style.backgroundColor="transparent";
+}
+function hospitality(){
+    document.getElementById("bt1").style.backgroundColor="transparent";
+    document.getElementById("bt2").style.backgroundColor="rgba(255,1,136,255)";
+    document.getElementById("bt3").style.backgroundColor="transparent";
+    document.getElementById("bt4").style.backgroundColor="transparent";
+    document.getElementById("bt5").style.backgroundColor="transparent";
+    document.getElementById('fun2').scrollIntoView({ behavior: 'smooth' });
+}
+function transport(){
+    document.getElementById("bt1").style.backgroundColor="transparent";
+    document.getElementById("bt2").style.backgroundColor="transparent";
+    document.getElementById("bt3").style.backgroundColor="rgba(255,1,136,255)";
+    document.getElementById("bt4").style.backgroundColor="transparent";
+    document.getElementById("bt5").style.backgroundColor="transparent";
+    document.getElementById('fun3').scrollIntoView({ behavior: 'smooth' });
+}
+function events(){
+    document.getElementById("bt1").style.backgroundColor="transparent";
+    document.getElementById("bt2").style.backgroundColor="transparent";
+    document.getElementById("bt3").style.backgroundColor="transparent";
+    document.getElementById("bt4").style.backgroundColor="rgba(255,1,136,255)";
+    document.getElementById("bt5").style.backgroundColor="transparent";
+    document.getElementById('fun1').scrollIntoView({ behavior: 'smooth' });
+}
+function contact(){
+    document.getElementById("bt1").style.backgroundColor="transparent";
+    document.getElementById("bt2").style.backgroundColor="transparent";
+    document.getElementById("bt3").style.backgroundColor="transparent";
+    document.getElementById("bt4").style.backgroundColor="transparent";
+    document.getElementById("bt5").style.backgroundColor="rgba(255,1,136,255)";
+    document.getElementById('fun4').scrollIntoView({ behavior: 'smooth' });
+}
